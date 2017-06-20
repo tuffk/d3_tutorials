9592028dbf0c0b2d7440168b4583c62f2c7620b5
Tuffk <tuffkmulhall@gmail.com>
2017-06-20 23:17:13 +0300

    ok no tengo idea de como llamar mis variables

--- rubystuff/descargamela.rb
+++ rubystuff/descargamela.rb
@@ -1,4 +1,6 @@
 require 'open-uri'
+require 'nokogiri'
+require 'json'
 require 'pry'
 require 'pry-nav'
 files = Dir['*.svg']
@@ -12,3 +14,42 @@ if files.empty?
   end
   puts "#{num} files downloaded"
 end
+
+json = File.open('bigjson.json', 'w+')
+vagina = {}
+files.each do |f|
+  File.open(f, 'r') do |doc|
+    name = File.basename(doc)
+    vagina[name] = {}
+    verga = 0
+    data = File.open(doc) { |fi| Nokogiri::XML(fi) }
+    data.search('path').each do |sharmuta|
+      vagina[name][verga.to_s] = {}
+      title = sharmuta['title']
+      path = sharmuta['d']
+      vagina[name][verga.to_s]['name'] = title
+      vagina[name][verga.to_s]['path'] = path
+      verga += 1
+    end
+  end
+end
+# vagina = {}
+# files.each do |f|
+#   File.open(f, 'r') do |doc|
+#     country = File.basename(doc)
+#     vagina[country] = {}
+#     doc.each_line do |l|
+#       if l.include? '<path'
+#         pito = l.split(' ')
+#         title = pito[2].tr('=', ':')
+#         title.insert(0, '"')
+#         title.insert(6, '"')
+#         vagina[country]['name'] = title
+#         vagina[country]['path'] = l
+#       else
+#         next
+#       end
+#     end
+#   end
+# end
+json << vagina.to_json
