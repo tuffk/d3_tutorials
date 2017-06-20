dc21b2a972c57513521c3c3632ae94eb6d799a18
Tuffk <tuffkmulhall@gmail.com>
2017-06-20 21:38:38 +0300

    download part

--- rubystuff/descargamela.rb
+++ rubystuff/descargamela.rb
@@ -1,6 +1,14 @@
 require 'open-uri'
+require 'pry'
+require 'pry-nav'
+files = Dir['*.svg']
 countries = %w[israel mexico]
-countries.each do |c|
-  download = open("https://www.amcharts.com/lib/3/maps/svg/#{c}High.svg")
-  IO.copy_stream(download, "#{c}.svg")
+num = 0
+if files.empty?
+  countries.each do |c|
+    download = open("https://www.amcharts.com/lib/3/maps/svg/#{c}High.svg")
+    IO.copy_stream(download, "#{c}.svg")
+    num += 1
+  end
+  puts "#{num} files downloaded"
 end
